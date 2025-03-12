9263e55e3af9fe8a636df7051c42be7e1ead4668
alfredmagat23 <alfredmagat222@gmail.com>
2025-03-12 20:28:04 +0800

    added login and signup design.

--- VehicleRental/wwwroot/js/site.js
+++ VehicleRental/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//added password toggle
+    function togglePassword(inputId) {
+                            const input = document.getElementById(inputId);
+    const button = input.nextElementSibling.querySelector('i');
+
+    if (input.type === 'password') {
+        input.type = 'text';
+    button.classList.replace('bi-eye', 'bi-eye-slash');
+                            } else {
+        input.type = 'password';
+    button.classList.replace('bi-eye-slash', 'bi-eye');
+                            }
+                                                }
