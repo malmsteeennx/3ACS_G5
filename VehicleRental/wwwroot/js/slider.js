d2505b5b9349ffdfa42386e298a0589c09f492c7
Tej <jetdelaremalol@gmail.com>
2025-03-18 18:49:49 +0800

    Adjusted "ABOUT US" Section

--- VehicleRental/wwwroot/js/slider.js
+++ VehicleRental/wwwroot/js/slider.js
@@ -1,7 +1,5 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
     const slides = document.querySelectorAll('.about-us-slide');
-    const prevButton = document.querySelector('.prev-slide');
-    const nextButton = document.querySelector('.next-slide');
     let currentSlide = 0;
 
     function showSlide(index) {
@@ -13,14 +11,18 @@
         });
     }
 
-    prevButton.addEventListener('click', function () {
-        currentSlide = (currentSlide > 0) ? currentSlide - 1 : slides.length - 1;
-        showSlide(currentSlide);
+    document.querySelectorAll('.prev-slide').forEach(button => {
+        button.addEventListener('click', function () {
+            currentSlide = (currentSlide > 0) ? currentSlide - 1 : slides.length - 1;
+            showSlide(currentSlide);
+        });
     });
 
-    nextButton.addEventListener('click', function () {
-        currentSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0;
-        showSlide(currentSlide);
+    document.querySelectorAll('.next-slide').forEach(button => {
+        button.addEventListener('click', function () {
+            currentSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0;
+            showSlide(currentSlide);
+        });
     });
 
     showSlide(currentSlide);
